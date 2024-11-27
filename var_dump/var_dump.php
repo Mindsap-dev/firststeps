b527cd97521fd3107ad22dc4ee4c5be12d17969b
Dennis <triaxxin29@gmail.com>
2024-11-26 20:24:13 -0600

    11/26/24

---
+++ var_dump/var_dump.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewerport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <title>Document</title>
+</head>
+
+<body>
+<pre><?php
+
+    var_dump('Hello from var_dump');
+    var_dump(123);
+    ?>
+
+    </pre>
+</body>
+</html>
